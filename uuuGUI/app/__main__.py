d4ddb7427e59f3bc686bf5e8b39ab391ff73fcbb
Tony Gao <jiahanbryant@gmail.com>
2022-12-16 16:02:14 +0800

    [uuuGUI] add program feature and progress bar, updated images

--- uuuGUI/app/__main__.py
+++ uuuGUI/app/__main__.py
@@ -127,22 +127,25 @@ def recover():
     try:
         cmdCheck(loadImage)
         if serial_setup(COM, 115200):
+            boot_timeout = time.time() + 60
             while True:
-                boot_timeout = time.time() + 60
+                y = format(ser.readline().decode("utf-8", "replace").rstrip())
+                if y != "":
+                    print(y)
 
-                y = ser.readline()
-                print("{}".format(y.decode("utf-8", "replace").rstrip()))
-
-                if b'test=>' in y:
+                if 'test=>' in y:
                     print('\nboot [OK]')
+                    window['-STA-'].update(30)
                     ser.write("flash boot".encode() + b'\n')
                     pass
-                elif b'flash boot [OK]' in y:
+                elif 'flash boot [OK]' in y:
                     print("now verifying...")
+                    window['-STA-'].update(70)
                     ser.write("verify".encode() + b'\n')
                     pass
-                elif b'verify [OK]' in y:
+                elif 'verify [OK]' in y:
                     print("Recovery Done successfully.")
+                    window['-STA-'].update(100)
                     global recovery
                     recovery = True
                     return
@@ -157,25 +160,74 @@ def recover():
     except:
         print("Error: failed to boot image.")
 
+# To program module from USB, file name has to be: image.swu on USB drive
+def program():
+    try:
+        if serial_setup(COM, 115200):
+            boot_timeout = time.time() + 120
+            global programmed
+            while True:
+                y = format(ser.readline().decode("utf-8", "replace").rstrip())
+                if y != "":
+                    print(y)
+
+                if 'Hit any key to stop autoboot' in y:
+                    window['-STA-'].update(1)
+                    print("Stopping in u-boot shell.")
+                    ser.write("".encode() + b'\n')
+                    pass
+                elif '=>' in y:
+                    print("Setting update parameters.")
+                    ser.write("setenv usb_update_req 1".encode() + b'\n')
+                    ser.write("run swu_flash_boot".encode() + b'\n')
+                    window['-STA-'].update(3)
+                    pass
+                elif 'SWUPDATE running' in y:
+                    # to move the progress bar as time goes by
+                    i = (1 - (boot_timeout - time.time())/120) * 100
+                    window['-STA-'].update(i)
+                    pass
+                elif '[swupdate-init]: INFO: No USB with update file found' in y:
+                    print("ERROR: NO valid image found from USB.")
+                    programmed = False
+                    return
+                elif 'SUCCESS !' in y:
+                    print("program done.")
+                    programmed = True
+                    window['-STA-'].update(100)
+                    return
+                elif time.time() > boot_timeout:
+                    print("ERROR: program time out.")
+                    programmed = False
+                    return
+        else:
+            print("check serial configuraton.")
+            programmed = False
+            return      
+    except:
+        print("[ERROR] program failed.")
+        programmed = False
+
 def main() -> None:
 
     # Window Layout
-    layout = [  [sg.Text('Device'), sg.Text('Device Not Connected', key='-DEVICE-', text_color='Red', size=(46,1)), sg.Button('Check', size=(6,1))],
+    layout = [  [sg.Text('Device'), sg.Text('Device Not Connected', key='-DEVICE-', text_color='Red', background_color='white', size=(46,1)), sg.Button('Check', size=(6,1))],
                 [sg.Input(sg.user_settings_get_entry('-filename-', ''), key='-IMAGEPATH-', size=(60,1)), sg.FolderBrowse('Browse', size=(6,1))],
                 [sg.Text('Select Serial'), sg.Combo([comport.device for comport in serial.tools.list_ports.comports()], default_value="Select Com Port", size=(10, 1), key='-COM-')],
-                [sg.Button('Recover')],
-                [sg.Output(size=(80,20), key='-OUTPUT-', expand_x=True, expand_y=True)],
-                [sg.Exit()]
+                [sg.Button('Recover', size=(6,1)), sg.Button('Program', size=(6,1)), sg.Text("Program status", key='-PROG-', size=(12,1)), sg.ProgressBar(100, size=(47,5), key='-STA-')],
+                [sg.Output(size=(100, 30), key='-OUTPUT-', expand_x=True, expand_y=True)],
+                #[sg.Exit()]
     ]
 
     # Create the Window
     global window
+    sg.theme('Light Blue 3')
     version = open(resource_path("version"), 'r+').read()
-    window = sg.Window(f'Stream1955 Recovery Tool (ver. {version})', layout, icon="icon/48x48.ico", resizable=True, element_justification='centre', finalize=True)
-    print("Use of instruction:\n1, Connect USB from PC to Device, check for device;\n2, [Opt.]Select files for loading;\n3, Select serial port;\n4, Press recover.")
+    window = sg.Window(f'Stream1955 Recovery Tool ver.{version}', layout, icon="icon/48x48.ico", resizable=True, element_justification='centre', finalize=True)
+    print("Use of instruction[Recover]:\n1, Connect USB from PC to Device, check for device;\n2, [Opt.]Select files for loading;\n3, Select serial port;\n4, Press \'Recover\'.")
+    print("\nUse of instruction[Program]:\n1, Select serial port;\n2, Press on button \'Program\';\n3, Power on device;")
 
-    global running_OS
-    global COM
+    global running_OS, COM
     running_OS = platform.system()  
 
     while True:
@@ -183,8 +235,12 @@ def main() -> None:
         if event in (sg.WIN_CLOSED, 'Exit'):
             break
         elif event == "Check":
+            window['-OUTPUT-'].update("")
             retrieveDEVICE()
         elif event == "Recover":
+            window['-STA-'].update(0, bar_color=('Green', 'White'))
+            window['-OUTPUT-'].update("")
+            window['-PROG-'].update(f"In progress", text_color="Brown")
             if not retrieveDEVICE():
                 print("Connect device first!")
                 continue
@@ -200,10 +256,32 @@ def main() -> None:
             else:
                 print("Please select correct com port.")
 
-        elif event == '-RECOVER DONE-' and recovery:
-            print("Recovery [OK].")
-            # Not in USB mode after recovery
-            window['-DEVICE-'].update(f"Device Not Connected", text_color="Red")
+        elif event == '-RECOVER DONE-':
+            if recovery:
+                window['-PROG-'].update(f"Recovery OK", text_color="Green")
+                # Not in USB mode after recovery
+                window['-DEVICE-'].update(f"Device Not Connected", text_color="Red")
+            else:
+                window['-PROG-'].update(f"Recovery Failed", text_color="Red")
+                window['-STA-'].update(0, bar_color='Red')
+
+        elif event == "Program":
+            window['-PROG-'].update(f"In progress", text_color="Brown")
+            window['-STA-'].update(0, bar_color=('Green', 'White'))
+            window['-OUTPUT-'].update("")
+            if values['-COM-'] != "Select Com Port":
+                COM = values['-COM-']
+                print("Using " + COM + "\nNow power cycle the device.")
+                window.perform_long_operation(program, '-PROGRAM DONE-')
+            else:
+                print("Please select correct com port.")
+
+        elif event == '-PROGRAM DONE-':
+            if programmed:
+                window['-PROG-'].update(f"Program OK", text_color="Green")
+            else:
+                window['-PROG-'].update(f"Program Failed", text_color="Red")
+                window['-STA-'].update(0, bar_color='Red')
 
 if __name__ == "__main__":
     main()
